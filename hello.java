cf42bb644dec0faf4ca76486b41760e7069b41e3
Chenziyang7799 <1970237486@qq.com>
2020-01-21 14:39:24 +0800

    first commit

---
+++ hello.java
@@ -0,0 +1,38 @@
+public class hello {
+    public static void main(String[] args) {
+        //int a = 1;
+        //int b = a+2;
+        //char a = 65;
+        //char a = 'a';       //必须用单引号
+        //String a = "aaaaa";   //必须用双引号
+        //System.out.println(a);
+        //System.out.println("hello java");
+        //final int a1 = 10;   // 加了final代表是最终变量或者称为常量
+        //a1 = 20;
+        //System.out.println(a1);
+        //short a = 1;
+        //short b = 1;
+        //short c = a + b;  // 两个short相加为int型，赋值为比int型小的short型会报错
+        //double c = a + b; // 因为double型比int型长，所以不会报错
+        //System.out.println(c);
+        //short a = 2;
+        //short b = +a;  // 报错是因为一元加会把类型变长，使short型变为int型，赋值为short会报错、
+        //int b = +a; //不会报错
+        //Float a = 1.0f;
+        //Float b = 1.0f;
+        //System.out.println(a == b);//输出false，因为Flort和Double两个包装类是没有缓存区的，直接创建new，不同地址所以用==比较地址时肯定不相同
+        //int a = 10000; //赋值10000时，因为short型可以包含这个值，所以转化为short型后可以正常显示
+        //int a = 100000;//赋值100000时，因为short型不包含这个值，所以转化为short型后不能正常显示
+        //short b = a; //报错是因为不能从int型直接赋值给short型
+        //short b = (short)a;
+        //System.out.println(b);
+
+        //Integer n3 = 100; //Integer是包装类型，int是基本类型
+        //Integer n3 = Integer.valueOf("100") //javac时，也就是编译时会把上面的语句翻译成这一句进行编译
+        //System.out.println(0.1+0.1==0.2);//浮点型是不精确的，乘法运算时是不精确的，加法是可以的
+    }
+
+
+
+
+}
